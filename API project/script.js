fb90670288463020ce6010912676b980bc655b2e
vasudevtank0901 <vasudevtank0901@gmail.com>
2026-04-03 14:04:15 +0530

    Update script.js

--- API project/script.js
+++ API project/script.js
@@ -1,32 +1,5 @@
 let container = document.getElementById("container")
 
-// const fetchApi = async ()=>{
-//     let response = await fetch("https://dummyjson.com/products")
-//     let res = await response.json()
-
-//     res.product.forEach((product)=>{
-//         let div = document.createElement("div")
-//         let p = document.createElement("p")
-//         let img = document.createElement("img")
-        
-//         img.src = product.images[0]
-
-//         div.setAttribute("class","product")
-
-//         p.innerText = product.description
-
-//         div.append(p)
-//         div.append(img)
-
-//         container.append(div)
-
-//         console.log(product);
-        
-//     })
-// }
-
-// fetchApi()
-
 async function getProducts() {
     let response = await fetch("https://dummyjson.com/products")
     let res = await response.json()
